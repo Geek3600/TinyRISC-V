dd14e4152569e78c05a0ab87139b1abb601b8fd8
Geek3600 <2741057934@qq.com>
2024-11-12 18:55:58 +0800

    test

--- rtl/core/pc_reg.v
+++ rtl/core/pc_reg.v
@@ -23,19 +23,20 @@ module pc_reg(
     input wire rst,
 
     input wire jump_flag_i,                 // 跳转标志
-    input wire[`InstAddrBus] jump_addr_i,   // 跳转地址
-    input wire[`Hold_Flag_Bus] hold_flag_i, // 流水线暂停标志
+    input wire[`InstAddrBus] jump_addr_i,   // 跳转地址，即跳转到该地址
+    input wire[`Hold_Flag_Bus] hold_flag_i, // 流水线暂停标志，即pc值保持不变
     input wire jtag_reset_flag_i,           // 复位标志
 
-    output reg[`InstAddrBus] pc_o           // PC指针
+    output reg[`InstAddrBus] pc_o           // PC指针指
 
     );
 
 
     always @ (posedge clk) begin
         // 复位
+        // 外部复位或者是jtag复位
         if (rst == `RstEnable || jtag_reset_flag_i == 1'b1) begin
-            pc_o <= `CpuResetAddr;
+            pc_o <= `CpuResetAddr;// 一个全0地址
         // 跳转
         end else if (jump_flag_i == `JumpEnable) begin
             pc_o <= jump_addr_i;
